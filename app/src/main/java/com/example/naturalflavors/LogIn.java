52ffc4cc13321d723f7241ddad8d9a2cd407306a
alexisrolenc <alexis.rolenc@gmail.com>
2019-04-13 11:26:11 -0500

    committed by alexis

---
+++ app/src/main/java/com/example/naturalflavors/LogIn.java
@@ -0,0 +1,23 @@
+package com.example.naturalflavors;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LogIn extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+    }
+
+    public void onLogInButtonPressed (View v)
+    {
+        Intent i = new Intent (this, MainActivity.class);
+        this.startActivity(i);
+    }
+}

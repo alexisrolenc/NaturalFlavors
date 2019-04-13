eabd02837c70d0127a180c7c8bb9f6da34a7a4ee
Sylaedriel <schramdawg@gmail.com>
2019-04-13 15:42:38 -0500

    tms signup button

--- app/src/main/java/com/example/naturalflavors/Core.java
+++ app/src/main/java/com/example/naturalflavors/Core.java
@@ -2,10 +2,12 @@ package com.example.naturalflavors;
 
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Core
 {
+    //static FirebaseAuth mAuth;
     static FirebaseUser currentUser;
     static Integer salary;
     static String fName;
